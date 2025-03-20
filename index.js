c9bfa251701f1be58791abc5fcb6136c91fccf71
UBG85 <lorax.trees.sthe@gmail.com>
2025-03-20 21:29:36 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementById('downloadForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const url = document.getElementById('url').value;
+    fetch('/download', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: url })
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'ranxlus ytm.mp3';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    })
+    .catch(error => console.error('Error:', error));
+});
